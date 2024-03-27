1dd5fc3e36414888f518550c7b443c20f33b0559
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-03-27 19:26:18 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "main.hpp"
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  std::cout << "Hello world part3" << std::endl;
+  return EXIT_SUCCESS;
+}
