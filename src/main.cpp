67221efdae0b3dde74226d7441dc6f1478d4d2e0
Francesco Virgulti <45544719+francescoVirgulti@users.noreply.github.com>
2024-03-27 19:47:52 +0100

    aggiornameno 2

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,7 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   std::cout << "Hello world part4" << std::endl;
   std::cout << "Hello world partFra" << std::endl;
+  std::cout << "Hello world partFra1" << std::endl;
+  
   return EXIT_SUCCESS;
 }
