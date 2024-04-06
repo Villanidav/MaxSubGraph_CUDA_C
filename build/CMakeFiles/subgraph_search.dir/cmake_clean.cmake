d596f31e5d4c776d6d70a40866cc4295497d0811
francescoVirgulti <franci.virgulti@gmail.com>
2024-04-06 18:28:57 +0200

    spiegazione di Davide, aggiornamento Git

---
+++ build/CMakeFiles/subgraph_search.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subgraph_search.dir/src/main.cpp.o"
+  "CMakeFiles/subgraph_search.dir/src/main.cpp.o.d"
+  "subgraph_search"
+  "subgraph_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subgraph_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
