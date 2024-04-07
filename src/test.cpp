9ce4e58ad6ca2f0eebcf1c7f469314fddcb1e313
Villanidav <davidevillani2002@gmail.com>
2024-04-07 02:00:52 +0200

    RISOLTO PROBLEMA test:
    - inserito modifiche nel CMakeLists.txt : cambiato source_files e aggiunti i file di test.cpp manualmente
    - buildando ora nella directory build saranno presente un file di test e uno subgraph_search, in riferimento a test e main
    
    TODO: fare una funzione test per fra

---
+++ src/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "test.hpp"
+
+
+int main() {
+    std::string input = "example";
+    std::vector<std::pair<std::string, int>>  result = gen_rotations(input);
+
+    // Printing the result
+    for (const auto& pair : result) {
+        std::cout << pair.first << ", " << pair.second << std::endl;
+    }
+
+    return 0;
+}
---
+++ src/test.hpp
@@ -0,0 +1,4 @@
+#include <string>
+#include <vector>
+
+std::vector<std::pair<std::string, int>> gen_rotations(const std::string& s);
