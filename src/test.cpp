0d182fb64cf23e0a4a38524242affdb66c3a765f
Villanidav <davidevillani2002@gmail.com>
2024-04-07 02:20:37 +0200

    ESEGUITO TEST SU gen_rotations : passed

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,7 @@
 int main() {
     std::string input = "example";
     std::vector<std::pair<std::string, int>>  result = gen_rotations(input);
-
+ 
     // Printing the result
     for (const auto& pair : result) {
         std::cout << pair.first << ", " << pair.second << std::endl;
