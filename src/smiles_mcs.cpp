e22ea31c685ceda5a752ccb3df0a290b08e22e5d
Villanidav <davidevillani2002@gmail.com>
2024-04-10 18:55:01 +0200

    aggiornamento

--- src/smiles_mcs.cpp
+++ src/smiles_mcs.cpp
@@ -1,10 +1,9 @@
-/*
-
 #include <RDGeneral/Invariant.h>
 #include <GraphMol/RWMol.h>
 #include <GraphMol/ROMol.h>
 #include <GraphMol/Conformer.h>
 
-using namespace RDKit; */
+using namespace RDKit; 
 
 //['/home/davide/miniconda3/envs/my-rdkit-env/lib/python3.12/site-packages/rdkit'] 
+
--- src/test.hpp
+++ src/test.hpp
@@ -3,9 +3,9 @@
 #include "Label.cpp"
 using namespace std; // Add this line
 
-std::vector<std::pair<std::string, int>> gen_rotations(const std::string& s);
+std::vector<std::pair<std::string, int> > gen_rotations(const std::string& s);
 
-int select_vertex(std::vector<int>& vtx_set, std::vector<std::vector<int>>& g);
+int select_vertex(std::vector<int>& vtx_set, std::vector<std::vector<int> >& g);
 
-std::vector<int> hood(int vtx, const std::vector<std::vector<int>>& g, int edge);
+std::vector<int> hood(int vtx, const std::vector<std::vector<int> >& g, int edge);
 
